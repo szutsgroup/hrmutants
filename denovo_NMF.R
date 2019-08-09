77b48ebab55eee6f4d0fdacbc065a34f69e4c642
szutsgroup <53616195+szutsgroup@users.noreply.github.com>
2019-08-09 16:21:24 +0200

    Add files via upload

---
+++ denovo_NMF.R
@@ -0,0 +1,75 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww16060\viewh17860\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 library(tidyverse)\
+library(superheat)\
+library(MutationalPatterns)\
+library(BSgenome.Ggallus.UCSC.galGal4)\
+library(BSgenome.Hsapiens.NCBI.GRCh38)\
+\
+\
+#reading in postprocessed SNV data\
+snv <- read.delim("all_SNVs_postprocessed.isomut", stringsAsFactors = FALSE)\
+\
+\
+#creating a decoy vcf object\
+vcf_list <- vector(mode = "list", length = length(unique(snv$genotype)))\
+names(vcf_list) <- unique(snv$genotype)\
+\
+for (i in unique(snv$genotype)) \{\
+  t <- filter(snv, genotype == i)\
+  \
+  vcf_list[[i]] <- with(t, GRanges(seqnames = paste0("chr", chr), ranges = IRanges(start = pos, end = pos), seqinfo = seqinfo(Ggallus)))\
+  names(vcf_list[[i]] ) <- paste0("chr", t$chr, ":", t$pos, "_", t$ref, "/", t$mut)\
+  mcols(vcf_list[[i]] )$paramRangeID <- factor(NA)\
+  mcols(vcf_list[[i]] )$REF <- DNAStringSet(t$ref)\
+\
+  mcols(vcf_list[[i]] )$ALT <- DNAStringSetList(unname(sapply(t$mut, DNAStringSet)))\
+  mcols(vcf_list[[i]] )$QUAL <- as.numeric(NA)\
+  mcols(vcf_list[[i]])$FILTER <- "PASS"\
+\}\
+\
+#estimating a suitable component number\
+mut_mat <- mut_matrix(vcf_list = vcf_list, ref_genome = ref_genome)\
+mut_mat <- mut_mat + 0.0001\
+estimate <- nmf(mut_mat, rank=1:5, method="brunet", nrun=10, seed=123456)\
+plot(estimate)\
+ggsave(file = "SNV_plots/denovo_NMF/NMF_component_number_test.pdf", width = 8, height = 5)\
+\
+#2 components seem to be OK\
+#extracting 2 components\
+nmf_res_2 <- extract_signatures(mut_mat, rank = 2, nrun = 100)\
+colnames(nmf_res_2$signatures) <- c("Signature A", "Signature B")\
+rownames(nmf_res_2$contribution) <- c("Signature A", "Signature B")\
+\
+#saving some plots and numeric data\
+plot_96_profile(nmf_res_2$signatures, condensed = TRUE, ymax = 0.075)\
+ggsave("SNV_plots/denovo_NMF/NMF_denovo_2comp.pdf",  width = 8, height = 5)\
+\
+plot_contribution(nmf_res_2$contribution, nmf_res_2$signatures, mode = "absolute")\
+ggsave("SNV_plots/denovo_NMF/NMF_denovo_comparison.pdf", width = 8, height = 5)\
+\
+write.table(mut_mat, file = "triplet_mutation_matrix.txt", sep = "\\t", quote = FALSE, row.names = FALSE)\
+\
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+\cf0 write.table(nmf_res_2$contribution, file = "extracted_signatures.txt", sep = "\\t", quote = FALSE, row.names = FALSE)\
+\
+#"humanizing" the denovo components (correcting for differences between human and chicken triplet frequencies)\
+humanized <- nmf_res_2$signatures / (triplets96$Gallus.gallus * sum(as.numeric(seqlengths(Ggallus)))) * (triplets96$Homo.sapiens * sum(as.numeric(seqlengths(Hsapiens))))\
+\
+\
+#cosine similaryity and Spearman correlation\
+cos_sim_denovo_signatures_hum = cos_sim_matrix(humanized, cancer_signatures)\
+cancer_signatures <- read.delim("https://cancer.sanger.ac.uk/cancergenome/assets/signatures_probabilities.txt", stringsAsFactors = FALSE)[,1:33]\
+cos_sim_denovo_signatures_hum %>% \
+    superheat( bottom.label.text.angle = 90, bottom.label.text.size = 4, title = "Cosinus similarity", bottom.label.size = .6, left.label.size = .3, left.label.text.size = 4, legend.breaks = seq(0, 1, by = .2), heat.lim = c(0, 1))\
+\
+cor(humanized, cancer_signatures, method = "spearman") %>% \
+    superheat( bottom.label.text.angle = 90, bottom.label.text.size = 4, title = "Spearman correlation", bottom.label.size = .6, left.label.size = .3, left.label.text.size = 4)\
+\
+\
+\
+}
---
+++ isomut_postprocessing.R
@@ -0,0 +1,108 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww19860\viewh16600\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 # Isomut postprocessing\
+\
+# Aim: find a Fischer score tresholds where at most 5 SNVs, at most 1 deletions and at most 1 insertions are left in any of the starting clones\
+\
+library(tidyverse)\
+library(BSgenome.Ggallus.UCSC.galGal4)\
+\
+\
+#treatments\
+treat_list <- list(starting_clone = c("HR5", "HR21", "HR1", "HR9", "HR29", \
+                                   "HR33", "HR13", "HR17", "HR37", "HR25", \
+                                   "HR41"),\
+                             mock = c("HR6", "HR7", "HR8", "HR22", "HR23", \
+                                      "HR24", "HR2", "HR2", "HR3", "HR10", \
+                                      "HR11", "HR12", "HR30", "HR31", "HR32", \
+                                      "HR34", "HR35", "HR36", "HR14", "HR15", \
+                                      "HR16", "HR18", "HR19", "HR20", "HR38", \
+                                      "HR39", "HR40", "HR26", "HR27", "HR28", \
+                                      "HR42", "HR43", "HR44"))\
+#genotypes\
+geno_list <- list(WT = c("HR1", "HR2", "HR3", "HR4"),\
+                BRCA1 = c("HR5", "HR6", "HR7", "HR8"),\
+                BRCA2 = c("HR21", "HR22", "HR23", "HR24"),\
+              RAD51C = c("HR9", "HR10", "HR11", "HR12"),\
+               RAD52 = c("HR29", "HR30", "HR31", "HR32"),\
+               RAD54 = c("HR33", "HR34", "HR35", "HR36"),\
+               XRCC2 = c("HR13", "HR14", "HR15", "HR16"),\
+               XRCC3 = c("HR17", "HR18", "HR19", "HR20"),\
+                 ATM = c("HR37", "HR38", "HR39", "HR40"),\
+                CHK2 = c("HR41", "HR42", "HR43", "HR44"),\
+               PALB2 = c("HR25", "HR26", "HR27", "HR28"))\
+\
+\
+#reading in isomut SNV output file\
+snv <- read.delim("isomut_output/all_SNVs.isomut")\
+snv$sample <- substr(snv$X.sample_name, 0, 5)\
+\
+#cumulative plot: SNV counts vs. Fischer score\
+csnv <- matrix(0, ncol = length(table(snv$sample)), nrow = 301)\
+for (i in 1:301) \{ \
+    csnv[i,] <- as.vector(tabulate(factor(filter(snv, score > (i-1)/10)$X.sample_name, levels = names(table(snv$X.sample_name))), nbins = length(table(snv$sample)))) \
+\}\
+csnv <- data.frame(csnv, stringsAsFactors = FALSE)\
+names(csnv) <- names(table(snv$sample))\
+csnv$treshold <- seq(0, 30, by = 0.1)\
+csnvm <- gather(csnv, )\
+csnvm$status <- ifelse(csnvm$variable %in% treat_list$starting_clone, "starting clone", "other")\
+ggplot(data = csnvm, aes(x = treshold, y = log(value, base = 10), group = variable, color = status)) + geom_line() + ylab("log10 (Mutation number) ") + xlab("Score treshold") + geom_hline(yintercept = log10(5), linetype = 2)\
+\
+#calculate suitable Fischer treshold\
+nvScore <- csnv[ ,c(treat_list$starting_clone, "treshold")] %>% \
+  apply(1, function(x) ifelse(sum(x[1:length(treat_list$starting_clone)] < 5) == length(treat_list$starting_clone), x[length(treat_list$starting_clone) + 1], NA)) %>% \
+  min(na.rm = TRUE)\
+\
+#filtered snv set\
+snv_filt <- filter(snv, score > snvScore, chr != "MT")\
+snv_filt$genotype <- NA\
+for (i in 1:length(geno_list)) \{\
+  snv_filt[which(snv_filt$sample %in% geno_list[[i]]), "genotype"] <- names(geno_list)[i]\
+\}\
+snv_filt$treatment <- NA\
+for (i in 1:length(treat_list)) \{\
+  snv_filt[which(snv_filt$sample %in% treat_list[[i]]), "treatment"] <- names(treat_list)[i]\
+\}\
+\
+\
+\
+#reading in isomut indel output file\
+indel <- read.delim(file.path(outDir, "all_indels.isomut"))\
+indel$sample <- substr(indel$X.sample_name, 0, 5)\
+\
+#Fischer scores separately for insertions and deletions\
+ins <- filter(indel, type == "INS")\
+cins <- matrix(0, ncol = samplenum, nrow = 201)\
+for (i in 1:201) \{ \
+    cins[i,] <- as.vector(tabulate(factor(filter(ins, score > (i-1)/10)$sample, levels = names(table(indel$sample))), nbins = samplenum)) \
+\}\
+cins <- data.frame(cins, stringsAsFactors = FALSE)\
+names(cins) <- names(table(indel$sample))\
+cins$treshold <- seq(0, 20, by = 0.1)\
+cinsm <- melt(cins, id = "treshold")\
+cinsm$status <- ifelse(cinsm$variable %in% setdiff(treat_list$starting_clone, problematic), "starting clone", "other")\
+ggplot(data = cinsm, aes(x = treshold, y = log(value, base = 10), group = variable, color = status)) + geom_line() + ylab("log10 (Insertion number)") + xlab("Score treshold") + geom_hline(yintercept = log10(1), linetype = 2)\
+\
+del <- filter(indel, type == "DEL")\
+cdel <- matrix(0, ncol = samplenum, nrow = 201)\
+for (i in 1:201) \{ \
+    cdel[i,] <- as.vector(tabulate(factor(filter(del, score > (i-1)/10)$sample, levels = names(table(indel$sample))), nbins = samplenum)) \
+\}\
+cdel <- data.frame(cdel, stringsAsFactors = FALSE)\
+names(cdel) <- names(table(indel$sample))\
+cdel$treshold <- seq(0, 20, by = 0.1)\
+cdelm <- melt(cdel, id = "treshold")\
+cdelm$status <- ifelse(cdelm$variable %in% setdiff(treat_list$starting_clone, problematic), "starting clone", "other")\
+ggplot(data = cdelm, aes(x = treshold, y = log(value, base = 10), group = variable, color = status)) + geom_line() + ylab("log10 (Deletion number)") + xlab("Score treshold") + geom_hline(yintercept = log10(1), linetype = 2)\
+\
+\
+write.table(snv_filt, file = "all_SNVs_postprocessed.isomut", sep = "\\t", row.names = FALSE, quote = FALSE)\
+write.table(del_filt, file = "all_DELs_postprocessed.isomut", sep = "\\t", row.names = FALSE, quote = FALSE)\
+write.table(ins_filt, file = "all_INSs_postprocessed.isomut", sep = "\\t", row.names = FALSE, quote = FALSE)\
+\
+}
